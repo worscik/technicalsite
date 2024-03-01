499607e4023fbd107c63be504ca91ea32cf622e4
worscik <58079969+worscik@users.noreply.github.com>
2024-03-01 18:48:28 +0100

    project initiation
    project initiation

---
+++ src/test/java/pl/technicalsite/TechnicalsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.technicalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
